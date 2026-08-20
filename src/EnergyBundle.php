6ba9b7c032a263ab7555f45f5c1fb79080d7542a
92radar <viepro.db@gmail.com>
2026-08-20 14:28:46 +0200

    feat: Reintroduce EnergyBundleService class with build method

--- src/EnergyBundle.php
+++ src/EnergyBundle.php
@@ -5,7 +5,7 @@ namespace aintreallydown\EnergyBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class EnergyBundleService extends Bundle
+class EnergyBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
